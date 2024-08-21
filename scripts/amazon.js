3eae572992f59ed5537374015affc4726c4c59fc
Pratik Naik <prtknk13@gmail.com>
2024-08-21 12:38:30 +0530

    Moe addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,11 +1,11 @@
-import {cart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
 
 
 let productsHTML = '';
 
 products.forEach((product) => {
-  productsHTML += `
+    productsHTML += `
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -59,39 +59,25 @@ products.forEach((product) => {
 
 });
 
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+// function updating cart quantity
+function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((CartItem) => {
+        cartQuantity += CartItem.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      // console.log('Added product');
-      // console.log(button.dataset.productName);
-      const productId = button.dataset.productId;
-
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-
-      let cartQuantity = 0;
-
-      cart.forEach((item)=>{
-        cartQuantity = item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-    })
-  });
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+            addToCart(productId);
+            updateCartQuantity();
+        })
+    });
